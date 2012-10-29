dc1e01bf4286a87c3a11618e3290cc0170e9ee7a
andrew <andrew@andrew-desktop.(none)>
2012-10-30 02:13:22 +0300

    added favicon proccesing

---
+++ faviconHandler.js
@@ -0,0 +1,27 @@
+var fs = requeir('fs');
+var mime = requeir('mime');
+
+exports.handle = function(req, res){
+	if (res.method !== 'GET'){
+		res.writeHead(404, { 'Content-Type' : 'text/plain' });
+		res.end('invalid method ' + req.method);
+	} else if (req.basicServer.container.options.iconPath !== undefined) {
+		fs.readFile(req.basicServer.container.options.iconPath, 
+			function(err, buf){
+				if (err){
+					res.writeHead(500, { 'Content-Type' : 'text/plain' });
+					res.end(req.basicServer.container.options.iconPath + 'not found');
+				} else {
+					res.writeHead(200, { 
+						'Content-Type' : mime.lookup(req.basicServer.container.options.iconPath),
+						'Content-Length' : buf.length
+						});
+					res.end(buf);
+				}
+			});
+	} else {
+		res.writeHead(404, { 'Content-Type' : 'text/plain' });
+		res.end('no favicon');		
+	}
+
+}
